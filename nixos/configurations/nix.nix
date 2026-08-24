e2fe5cd9e87b508a39630b51bf19a46fd0b18f54
Giovani Granero <ggmelone@gmail.com>
2026-08-24 11:43:39 -0300

    NixOS garbage collector + auto-optimise-store

--- nixos/configurations/nix.nix
+++ nixos/configurations/nix.nix
@@ -1,5 +1,14 @@
 { config, pkgs, lib, ... }:
 
 {
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix.settings = {
+    experimental-features = [ "nix-command" "flakes" ];
+    auto-optimise-store = true;
+  };
+
+  nix.gc = {
+    automatic = true;
+    dates = "12:03";
+    options = "--delete-older-than 7d";
+  };
 }
